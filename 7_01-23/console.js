d234c0fdcaa5e0ac34b38d0cde08036caeb31788
shomersaf <shomersaf@gmail.com>
2023-01-07 15:59:03 +0200

    commied

---
+++ 7_01-23/console.js
@@ -0,0 +1,11 @@
+
+let velocity = prompt("Enter the Velocity in km per hour");
+velocity=Number(velocity);
+let time = prompt("Enter the TIME in hours");
+time=Number (time);
+let distance = velocity*time;
+distance = parseInt(distance);
+//console.log(distance);
+alert("The distance is" +" "+distance+" km/h");
+
+ 
