e28238603dc4ae006d00fc3350681cec6b66342d
shomersaf <shomersaf@gmail.com>
2023-01-07 23:54:39 +0200

    first

--- 7_01-23/output.js
+++ 7_01-23/output.js
@@ -1,1 +1,23 @@
+const form = document.getElementById('form');
+
+function retrieveFormValue(event) {
+    event.preventDefault();
+    const velocity = form.querySelector('[name="velocity"]');
+    const time = form.querySelector('[name="time"]');
+
+var tm=time.value;
+var vel=velocity.value;
+
+var distance = tm*vel;
+var beginning = '<!DOCTYPE html><html lang="en"><head> <meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /> <meta name="viewport" content="width=device-width, initial-scale=1.0" /><link rel="stylesheet" href="styles.css" /><title>Distance Calculator</title></head> <body> <div class="container"><header> <h1>distance calculator</h1> </header>  <main>'; 
+var ending='</main><footer><h5>&copy;StaceyS 2023</h5> </footer> </div><script src="output.js"></script></body></html>';
+
+   
+
+    document.write(beginning+'<div class="result"><p>'+ 'The distance is '+distance+' km/h'+'</p></div>'+ending);
+}
+form.addEventListener('submit', retrieveFormValue);
+
+
+
 
