0acb4ad6c20822903b01d2b61aba3b99667662b4
shomersaf <shomersaf@gmail.com>
2023-02-07 20:54:23 +0200

    hw_6_02_ready

--- 06_02/index.js
+++ 06_02/index.js
@@ -1,23 +1,25 @@
 let counter = 1;
 
 function getOrder() {
+  //getting data
   let orderEmail = document.getElementById("orderEmail");
   let credit = document.getElementById("credit");
   let numberOfSeats = document.getElementById("numberOfSeats");
   let additionalComments = document.getElementById("additionalComments");
   let outside = document.getElementById("outside");
-  let allergens = document.querySelectorAll(".allergens")
- 
+  let allergens = document.querySelectorAll(".allergens");
 
+  //getting order number
+  let orderNumber = `${counter++}`;
+
+  //creating order block - ul
   const orderBlock = document.querySelector(".orderBlock");
   let ordersList = document.createElement("ul");
   ordersList.className = "ordersList";
   orderBlock.append(ordersList);
-  
-  let orderNumber = `${counter++}`;
 
-  let orderHeadline = document.createElement("h3");
-
+  //creating order lines elements - li
+  let orderHeadline = document.createElement("h4");
   let orderEmailLine = document.createElement("li");
   let orderCreditLine = document.createElement("li");
   let orderSeatsLine = document.createElement("li");
@@ -26,32 +28,34 @@ function getOrder() {
   let orderDateLine = document.createElement("li");
   let orderTimeLine = document.createElement("li");
   let orderAllergensLine = document.createElement("li");
-  
+
+  //entering data into order lines
+  orderHeadline.innerHTML = `<b>Order N#</b>${orderNumber}:`;
   orderEmailLine.innerHTML = `<b>Email:</b> ${orderEmail.value}`;
   orderCreditLine.innerHTML = `<b>Credit:</b> ${credit.value}`;
   orderSeatsLine.innerHTML = `<b>Number of Seats:</b> ${numberOfSeats.value}`;
   orderCommentsLine.innerHTML = `<b>Comments:</b> ${additionalComments.value}`;
-  
-  if(outside.checked==true){
+  //checking up the placement and entering data
+  if (outside.checked == true) {
     orderOutsideLine.innerHTML = `<b>Place:</b> Outside`;
   } else {
     orderOutsideLine.innerHTML = `<b>Place:</b> Inside`;
   }
-
+  //continueing to enter data into order lines
   orderDateLine.innerHTML = `<b>Date:</b> ${date.value}`;
   orderTimeLine.innerHTML = `<b>Time:</b> ${time.value}`;
- 
- let allergensString = " ";
-   for (let i=0; i<allergens.length; i++){
-    if(allergens[i].selected){
-      allergensString = allergensString + " " + allergens[i].value +",";
+  //checking up if an allergen selected and entering data
+  let allergensString = " ";
+  for (let i = 0; i < allergens.length; i++) {
+    if (allergens[i].selected) {
+      allergensString = allergensString + " " + allergens[i].value + ",";
     }
-   }
-  
+  }
   allergensString = allergensString.slice(0, -1);
   allergensString = allergensString.concat(".");
-   orderAllergensLine.innerHTML = `<b>Allergens:</b> ${allergensString}`
-  
+  orderAllergensLine.innerHTML = `<b>Allergens:</b> ${allergensString}`;
+
+  //assigning the class to the order lines
   orderEmailLine.className = "orderClass";
   orderCreditLine.className = "orderClass";
   orderSeatsLine.className = "orderClass";
@@ -61,35 +65,32 @@ function getOrder() {
   orderTimeLine.className = "orderClass";
   orderAllergensLine.className = "orderClass";
 
-  orderHeadline.innerHTML = `<b>Order N#</b>${orderNumber}:`;
-
-
+  //creating removal Button
   const removeButton = document.createElement("img");
-  removeButton.src =
-    "xmark-solid.svg";
-    removeButton.height = 50;
-    removeButton.width = 50;
-    removeButton.onclick = function () {
-      this.parentElement.remove(); 
-      //document.getElementsByClassName("orderClass").remove();
-    };
+  removeButton.src = "xmark-solid.svg";
+  removeButton.height = 40;
+  removeButton.width = 40;
+  removeButton.className = "removeButton";
+  removeButton.onclick = function () {
+    this.parentElement.remove();
+  };
 
-
-  ordersList.append(orderHeadline);
-  ordersList.append(orderEmailLine);
-  ordersList.append(orderCreditLine);
-  ordersList.append(orderSeatsLine);
-  ordersList.append(orderCommentsLine);
-  ordersList.append(orderDateLine);
-  ordersList.append(orderTimeLine);
-  ordersList.append(orderAllergensLine);
-  ordersList.append(orderOutsideLine);
-  ordersList.append(removeButton);
-  
-  
+  //publishing the New World Order)))
+  ordersList.append(
+    orderHeadline,
+    orderEmailLine,
+    orderCreditLine,
+    orderSeatsLine,
+    orderCommentsLine,
+    orderDateLine,
+    orderTimeLine,
+    orderAllergensLine,
+    orderOutsideLine,
+    removeButton
+  );
 }
 
-
+//cleaning the form data
 function clearForm() {
-    $(this).closest('form')[0].reset(); 
+  $(this).closest("form")[0].reset();
 }
