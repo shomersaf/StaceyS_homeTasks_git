a780a5ea093eb8b608459e8f0f7af3fe1c288ea2
shomersaf <shomersaf@gmail.com>
2023-02-20 20:31:11 +0200

    deeting in UI

--- 13_02_HW/COURSES/index.js
+++ 13_02_HW/COURSES/index.js
@@ -1,23 +1,25 @@
+//declaring
 let counter = 0;
 const sendButton = document.querySelector(".sendButton");
 sendButton.addEventListener("click", goGoText);
-
+const clearButton = document.querySelector(".clearButton");
+clearButton.addEventListener("click", clearForm);
 const container = document.querySelector(".container");
-
-
+//getting
+const form = document.forms["myForm"];
+//creating
 const table = document.createElement("table");
 const tHead = document.createElement("thead");
 const tHName = document.createElement("th");
-
-
+const tBody = document.createElement("tbody");
+//appending
 container.append(table);
-table.append(tHead);
+table.append(tHead, tHead);
 tHead.append(tHName);
 tHName.innerText = "Name";
 
 
-
-
+//functions
 
 function goGoText(){
 //declaring 
@@ -26,22 +28,31 @@ const tRow = document.createElement("tr");
 const tData1 = document.createElement("td");
 const tData2 = document.createElement("td");
 //getting
-const form = document.forms["myForm"];
+//const form = document.forms["myForm"];
 let cName = form.elements["courseName"];
 tData1.innerText = cName.value;
 //buttons
 const delButton = document.createElement("button");
 delButton.innerText = " X ";
 delButton.className = "delRowButton";
+delButton.addEventListener("click", delCourse)
 tData2.append(delButton);
 //appending
 tRow.append(tData1, tData2);
-table.append(tRow);
+tHead.append(tRow);
+tRow.id = counter;
 //cleaning form inputs
 cName.value = "";
 }
 
+function clearForm(){
+  let cName = form.elements["courseName"];
+  cName.value = "";
+}
 
+function delCourse(){
 
-
+ const toDelete =  this.closest('tr');
+ toDelete.remove();
+}
 
