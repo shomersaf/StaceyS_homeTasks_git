c996609092ad7cf2aa6f956e31791ba486b41ba0
shomersaf <shomersaf@gmail.com>
2023-02-21 16:14:12 +0200

    one input works

--- 13_02_HW/COURSES/index.js
+++ 13_02_HW/COURSES/index.js
@@ -1,4 +1,5 @@
 //declaring
+const courses =[];
 let counter = 0;
 const sendButton = document.querySelector(".sendButton");
 sendButton.addEventListener("click", goGoText);
@@ -10,36 +11,48 @@ const form = document.forms["myForm"];
 //creating
 const table = document.createElement("table");
 const tHead = document.createElement("thead");
-const tHName = document.createElement("th");
+const tHName1 = document.createElement("th");
+const tHName2 = document.createElement("th");
 const tBody = document.createElement("tbody");
 //appending
 container.append(table);
-table.append(tHead, tHead);
-tHead.append(tHName);
-tHName.innerText = "Name";
 
+
+tHName1.innerText = "Name";
+tHName2.innerText = "Actions";
 
 //functions
 
 function goGoText(){
 //declaring 
-counter++;
+tHead.append(tHName1, tHName2);
+table.append(tHead, tBody);
 const tRow = document.createElement("tr");
 const tData1 = document.createElement("td");
+tData1.className = "tData1";
 const tData2 = document.createElement("td");
 //getting
 //const form = document.forms["myForm"];
 let cName = form.elements["courseName"];
-tData1.innerText = cName.value;
+courses.push(cName);
+tData1.innerText = courses[counter].value;
+//tData1.innerText = cName.value;
 //buttons
 const delButton = document.createElement("button");
 delButton.innerText = " X ";
 delButton.className = "delRowButton";
 delButton.addEventListener("click", delCourse)
 tData2.append(delButton);
+//-------
+const editButton = document.createElement("button");
+  editButton.className = "editButton";
+  editButton.innerText = " edit ";
+  editButton.addEventListener("click", editCourse)
+  tData2.append(editButton);
 //appending
 tRow.append(tData1, tData2);
-tHead.append(tRow);
+tBody.append(tRow);
+counter++;
 tRow.id = counter;
 //cleaning form inputs
 cName.value = "";
@@ -51,8 +64,63 @@ function clearForm(){
 }
 
 function delCourse(){
-
  const toDelete =  this.closest('tr');
  toDelete.remove();
 }
+
+function editCourse(event){
+  const toEdit =  this.closest('tr');
+ toEdit.innerHTML =" ";
+ const newTd1 = document.createElement("td");
+ const newTd2 = document.createElement("td");
+ const newInput = document.createElement("input");
+ newInput.className ="newInput";
+ newInput.type ="text";
+ newInput.id ="enteredName";
+
+ const saveButton = document.createElement("button");
+ saveButton.type="submit";
+ saveButton.innerText ="save";
+ saveButton.classList.add("editButton", "saveButton");
+ saveButton.addEventListener("click", saveEntered);
+ newTd1.append(newInput);
+ newTd2.append(saveButton);
+ toEdit.append(newTd1, newTd2);
+ cName = document.querySelector("#enteredName");
+
+
+courses.splice[counter,1];
+courses.push(cName);
+counter++;
+ }
+function saveEntered(){
+ 
+  
+  const toEdit =  this.closest('tr');
+  toEdit.innerHTML =" ";
+  
+  //IT COMES IN TILL HERE
+ 
+  //alert(enteredName.value);
+  const newTd1 = document.createElement("td");
+  const newTd2 = document.createElement("td");
+  //newTd1.innerText = enteredName.value;
+ //alert(enteredName.value);
+ newTd1.innerText = courses[counter-1].value;
+ 
+  const delButton = document.createElement("button");
+  delButton.innerText = " X ";
+  delButton.className = "delRowButton";
+  delButton.addEventListener("click", delCourse)
+
+  const editButton = document.createElement("button");
+  editButton.className = "editButton";
+  editButton.innerText = " edit ";
+  editButton.addEventListener("click", editCourse)
+  newTd2.append(delButton, editButton);
+ 
+  toEdit.append(newTd1, newTd2);
+}
+
+
 
--- 13_02_HW/COURSES/readme.md
+++ 13_02_HW/COURSES/readme.md
@@ -1,8 +1,8 @@
 # Homework
 # support clear functionality in local storage
 # Create A web page that support Courses management
-# Add Course
-# Delete Course
+# Add Course +
+# Delete Course +
 # Edit Course
 # Filter Course
 # Course object: { name:"js" , price: "123$" , startDate: "DATE" , endDate:"DATE", lecture: "Rafi-Pafi" }
