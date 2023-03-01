4a1c8c914261f129f865b2acff28f5e7ba6c88a2
shomersaf <shomersaf@gmail.com>
2023-03-01 13:34:38 +0200

    ready

--- PROJECT/index.js
+++ PROJECT/index.js
@@ -8,7 +8,40 @@ const cleanButton = document.querySelector("#formCleaner");
 pinItButton.addEventListener("click", pinCard);
 cleanButton.addEventListener("click", cleanForm);
 
-let notes = [];
+function generateHTMLCard(newNote){
+  const card = document.createElement("div");
+  card.style.animation = "fade-in 1s";
+  card.classList.add("card", "col", "col-3");
+  const pin = document.createElement("img");
+  pin.className = "pin";
+  pin.src = 'img/knopka.png';
+  const note = document.createElement("div");
+  card.append(pin,note);
+  cardRow.append(card);
+  const topicText = document.createElement("div");
+  topicText.className = "topicText";
+  const cardFooter = document.createElement("div");
+  cardFooter.className = "cardFooter";
+  note.append(topicText,cardFooter);
+  const dateTime = document.createElement("div");
+  dateTime.className = "dateTime";
+  const cleanerButton = document.createElement("button");
+  cleanerButton.classList.add("btn", "btn-danger", "cleaner");
+  const iconDelete = document.createElement("i");
+  iconDelete.classList.add("bi", "bi-x-circle");
+  cleanerButton.append(iconDelete);
+  cleanerButton.addEventListener("click", delCard);
+  cardFooter.append(dateTime, cleanerButton);
+  const dateDiv = document.createElement("div");
+  dateDiv.className = "dateDiv";
+  const timeDiv = document.createElement("div");
+  timeDiv.className = "timeDiv";
+  dateTime.append(dateDiv, timeDiv);
+
+  topicText.innerText = newNote.noteTopic;
+  dateDiv.innerText = newNote.noteDate;
+  timeDiv.innerText = newNote.noteTime;
+}
 
 //functions
 function pinCard(){
@@ -18,49 +51,16 @@ function pinCard(){
     noteForm.elements["dateInput"].value != "" &&
     noteForm.elements["timeInput"].value != ""
   ){
-    const card = document.createElement("div");
-    card.style.animation = "fade-in 1s";
-    card.classList.add("card", "col", "col-3");
-    const pin = document.createElement("img");
-    pin.className = "pin";
-    pin.src = 'img/knopka.png';
-    const note = document.createElement("div");
-    card.append(pin,note);
-    cardRow.append(card);
-    const topicText = document.createElement("div");
-    topicText.className = "topicText";
-    const cardFooter = document.createElement("div");
-    cardFooter.className = "cardFooter";
-    note.append(topicText,cardFooter);
-    const dateTime = document.createElement("div");
-    dateTime.className = "dateTime";
-    const cleanerButton = document.createElement("button");
-    cleanerButton.classList.add("btn", "btn-danger", "cleaner");
-    const iconDelete = document.createElement("i");
-    iconDelete.classList.add("bi", "bi-x-circle");
-    cleanerButton.append(iconDelete);
-    cleanerButton.addEventListener("click", delCard);
-    cardFooter.append(dateTime, cleanerButton);
-    const dateDiv = document.createElement("div");
-    dateDiv.className = "dateDiv";
-    const timeDiv = document.createElement("div");
-    timeDiv.className = "timeDiv";
-    dateTime.append(dateDiv, timeDiv);
-     let newNote = {
+    let newNote = {
       noteTopic: document.querySelector("#topicInput").value,
       noteDate: document.querySelector("#dateInput").value,
       noteTime: document.querySelector("#timeInput").value,
     };
+    let notes = JSON.parse(localStorage.getItem("notes"));;
     notes.push(newNote);
     localStorage.setItem("notes", JSON.stringify(notes));
-    notes = JSON.parse(localStorage.getItem("notes"));
-    for(let i = 0; i < notes.length; i++){
-      topicText.innerText = notes[i].noteTopic;
-      dateDiv.innerText = notes[i].noteDate;
-      timeDiv.innerText = notes[i].noteTime;
-      cleanForm();
-    } 
-   
+    generateHTMLCard(newNote);
+    cleanForm();
   }else{
     alert("Fill EVERY field of the form, please!");
   } 
@@ -75,53 +75,13 @@ function cleanForm() {
 
 
 function genCard(){
-
- notes = JSON.parse(localStorage.getItem("notes")) || [];
-  if(notes.length > 0){
-    let newNote = {
-      cNumber: null,
-      noteTopic: null,
-      noteDate: null,
-      noteTime: null,
-    };
-    let notes=[];
-    notes.push(newNote);
-    notes = JSON.parse(localStorage.getItem("notes"));
-    for(let i = 0; i < notes.length; i++){
-      const card = document.createElement("div");
-      card.style.animation = "fade-in 1s";
-      card.classList.add("card", "col", "col-3");
-      const pin = document.createElement("img");
-      pin.className = "pin";
-      pin.src = 'img/knopka.png';
-      const note = document.createElement("div");
-      card.append(pin,note);
-      cardRow.append(card);
-      const topicText = document.createElement("div");
-      topicText.className = "topicText";
-      const cardFooter = document.createElement("div");
-      cardFooter.className = "cardFooter";
-      note.append(topicText,cardFooter);
-      const dateTime = document.createElement("div");
-      dateTime.className = "dateTime";
-      const cleanerButton = document.createElement("button");
-      cleanerButton.classList.add("btn", "btn-danger", "cleaner");
-      const iconDelete = document.createElement("i");
-      iconDelete.classList.add("bi", "bi-x-circle");
-      cleanerButton.append(iconDelete);
-      cleanerButton.addEventListener("click", delCard);
-      cardFooter.append(dateTime, cleanerButton);
-      const dateDiv = document.createElement("div");
-      dateDiv.className = "dateDiv";
-      const timeDiv = document.createElement("div");
-      timeDiv.className = "timeDiv";
-      dateTime.append(dateDiv, timeDiv);
-
-      topicText.innerText = notes[i].noteTopic;
-      dateDiv.innerText = notes[i].noteDate;
-      timeDiv.innerText = notes[i].noteTime;
-    }
-  
+  let notes = JSON.parse(localStorage.getItem("notes"));
+  if(notes == null){
+    notes = [];
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }
+  for(let i = 0; i < notes.length; i++){
+    generateHTMLCard(notes[i]);
   }
 }
 
@@ -131,37 +91,18 @@ function delCard(){
   const currentDate = currentCard.querySelector(".dateDiv");
   const currentTime = currentCard.querySelector(".timeDiv");
 
+  let notes = JSON.parse(localStorage.getItem("notes"));
 
-let newNote = {
-  noteTopic: null,
-  noteDate: null,
-  noteTime: null,
-};
-let notes=[];
-notes.push(newNote);
-
-notes = JSON.parse(localStorage.getItem("notes"));
-
-for(let index = 0; index < notes.length; index++){
-  if(currentTopic.innerText == notes[index].noteTopic &&
-    currentDate.innerText == notes[index].noteDate &&
-    currentTime.innerText == notes[index].noteTime){
-  notes.splice(index,1);
-   
-if(notes.length == 0){
-  notes = [];
-  localStorage.removeItem('notes');
-}else{
-  localStorage.setItem("notes", JSON.stringify(notes));
-}
-
+  for(let index = 0; index < notes.length; index++){
+    if(currentTopic.innerText == notes[index].noteTopic &&
+      currentDate.innerText == notes[index].noteDate &&
+      currentTime.innerText == notes[index].noteTime)
+    {
+      notes.splice(index,1);
+      localStorage.setItem("notes", JSON.stringify(notes));
+      break;
+    }
   }
- 
-  
-}
-
-
-currentCard.remove();
-
+  currentCard.remove();
 }
 
