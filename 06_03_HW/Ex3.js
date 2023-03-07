aa7089df51515b18e71efe4e9ce1dd226f4c643a
shomersaf <shomersaf@gmail.com>
2023-03-07 23:24:50 +0200

    Ex3validation

--- 06_03_HW/Ex3.js
+++ 06_03_HW/Ex3.js
@@ -1,9 +1,9 @@
-let currencyCode = prompt("Enter a currency code of three symbols");
+let currencyCode = prompt("Enter a currency code of three symbols. For example ILS for israeli shekel or USD for US dollar");
 
 function gerCounriesByCurrency(dataBase,curC){
     curC = curC.toUpperCase();
     var selectedCountries = [];
-    document.write(`<h3> The countries, using ${curC} as a currency are:</h3>`);
+    document.write(`<h3> The countries, using ${curC} currency are:</h3>`);
     dataBase.forEach( country => {
         if (country.currencies != null && country.currencies[curC] != null){
             selectedCountries.push(country) 
@@ -11,6 +11,10 @@ function gerCounriesByCurrency(dataBase,curC){
             console.log(country.name.common)
         }
     });   
+    if(selectedCountries.length == 0){
+        document.write(`<h3><i> none! </></h3> <p>Invalid data entered. Reload the page and enter the relevant data!</p>`);
+    }
+    
 }
 
 gerCounriesByCurrency(countries,currencyCode);
