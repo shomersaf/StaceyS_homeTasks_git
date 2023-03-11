12d7717f7d90fcf0abe22be844923bcdb7b8cdf3
shomersaf <shomersaf@gmail.com>
2023-03-11 22:09:02 +0200

    ready

--- 11_03_HW/index.js
+++ 11_03_HW/index.js
@@ -1,39 +1,9 @@
 function init() {
-  drawCountriesSelect();
-  drawCarsSelect();
   drawUsersSelect();
 }
 
 init();
 
-function drawCountriesSelect() {
-  const onlyCountriesNames = countries.map((currentCountry) => {
-    return {
-      text: currentCountry.name.common,
-      value: currentCountry?.flags?.svg,
-    };
-  });
-  const select = getSelect(onlyCountriesNames, drawFlag);
-  document.querySelector("#selectLocation").append(select);
-  drawFlag(select.value);
-}
-
-function drawCarsSelect() {
-  const carsa = cars.map((c) => {
-    return {
-      text: c.Name,
-      value: c.Acceleration,
-    };
-  });
-  const select = getSelect(carsa, drawAcceleration);
-  document.querySelector("#selectLocation").append(select);
-  drawAcceleration(select.value);
-}
-
-function drawAcceleration(acc) {
-  document.querySelector("#acc").innerHTML = "";
-  document.querySelector("#acc").innerHTML = acc;
-}
 
 function drawFlag(linkSrc) {
   const img = getImg(linkSrc);
@@ -46,7 +16,7 @@ function drawUsersSelect( ) {
   const ourUsers = users.map((u) => {
     return {
       text: u.email,
-      value: u?.picture?.medium,
+      value: u.picture?.medium,
     };   
   });
   const select = getSelect(ourUsers, drawUserPic);
@@ -58,4 +28,8 @@ function drawUserPic(picture) {
   const img = getImg(picture);
   document.querySelector("#pic").innerHTML = "";
   document.querySelector("#pic").append(img);
+
+  var user = users.find(u => u.picture?.medium == picture);
+  var country = countries.find(c => c.cca2 == user.nat);
+  drawFlag(country?.flags?.svg)
 }
