d944024f2b9a03845eb86274a50dff20f7667fe0
shomersaf <shomersaf@gmail.com>
2023-04-18 18:51:41 +0300

    confirmDelFromLS

--- PROJECT_2/js/ui-components/currencies.js
+++ PROJECT_2/js/ui-components/currencies.js
@@ -7,7 +7,7 @@ function getCurrencies() {
   h2.innerHTML = "Home &#9655;";
   const intro = document.createElement("div");
   intro.classList.add("intro");
-  const rules = `<span>for your attention:</span> the basic data and the additional info of 100 crypto coins presented below are rendomly generated on every load. Consider, please,that the non-commercial remote server used has a rate limit of 10-30 calls/minute. That's why the loading process seems to be longing too much sometimes and the progress bar looks stucked. There's non need to reload the page in such cases, just wait for automatic result. <br /> You may add <span>from 1 to 5 coins</span> to your personal real-time report list and watch the graphs. If you want to add the new one - come back to your report page and remove some of coins added before!`;
+  const rules = `<span>for your attention:</span> the basic data and the additional info of 100 crypto coins presented below are randomly generated on every load. Consider, please,that the non-commercial remote server used has a rate limit of 10-30 calls/minute. That's why the loading process seems to be longing too much sometimes and the progress bar looks stucked. There's no need to reload the page in such cases, just wait for automatic result. <br /> You may add <span>from 1 to 5 coins</span> to your personal real-time report list and watch the graphs. If you want to add the new one - come back to your report page and remove some of coins added before!`;
   intro.innerHTML = rules;
   const controllers = document.createElement("div");
   controllers.id = "controllers";
