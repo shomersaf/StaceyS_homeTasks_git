9fc9a27a52a9d4524e7b13c6aa477fe9f0f7edfd
shomersaf <shomersaf@gmail.com>
2023-05-07 12:15:12 +0300

    some react props studies

---
+++ react/my-album/src/Test.js
@@ -0,0 +1,11 @@
+import Test2 from  './Test2';
+
+function Test(){
+return(
+    <>
+    <h1>Test test! - component, based on fn</h1>
+    <Test2 />
+    </>
+);
+}
+export default Test;
---
+++ react/my-album/src/Test2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Test2 extends React.Component {
+  render(){
+    return(
+        <h2>Test 2 - component, based on class</h2>
+    )
+  }
+}
+export default Test2;
